3c5876347bd1e55dd94c1c9e3f3da77d0055660a
orbitoo <2098340707@qq.com>
2025-03-04 08:45:58 +0800

    刪除保活

--- main.py
+++ main.py
@@ -55,16 +55,16 @@ async def on_message(message):
         else:
             print(f"channel not found: {TARGET_CHANNEL_ID}")
 
-app = Flask('')
-@app.route('/')
-def home():
-    return "I'm alive"
-def run():
-    app.run(host='0.0.0.0', port=8080)
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
+# app = Flask('')
+# @app.route('/')
+# def home():
+#     return "I'm alive"
+# def run():
+#     app.run(host='0.0.0.0', port=8080)
+# def keep_alive():
+#     server = Thread(target=run)
+#     server.start()
 
-keep_alive()
+# keep_alive()
 
 client.run(TOKEN)
