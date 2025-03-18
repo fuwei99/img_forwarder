715645fb15956a2a1a4d05ccef47e6d29d6490e1
orbitoo <2098340707@qq.com>
2025-03-18 11:49:33 +0800

    类型提示

--- cogs/admin.py
+++ cogs/admin.py
@@ -3,11 +3,11 @@ import discord
 
 
 class Admin(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.hybrid_command(name="sync", description="Sync hybrid commands.")
     @commands.is_owner()
     async def sync(self, ctx: commands.Context):
-        self.bot.sync_all_commands()
+        self.bot.tree.sync()
         await ctx.send("Synced hybrid commands.", ephemeral=True, delete_after=5)
