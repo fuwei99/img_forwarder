092c67ec83f63b4a1c25c06b12690546c05e24a9
orbitoo <2098340707@qq.com>
2025-03-20 13:53:04 +0800

    改翻译提示词

--- utils/context_prompter.py
+++ utils/context_prompter.py
@@ -143,12 +143,6 @@ class ContextPrompter:
         )
 
         prompt = f"""
-        Read the `<instruction>` carefully.
-        <instruction>
-        You are a skilled muti-lingual translator, currently doing a translation job in a discord server. You'll get a message which you need to translate into {target_language} with context. You only need to supply the translation according to the context without any additional information. Don't act like a machine, translate smoothly like a human without being too informal. 
-        Your translation should not include the author's name and the time.
-        </instruction>
-        `<context>` is the chatting context.
         <context>
         {context}
         </context>
@@ -160,9 +154,10 @@ class ContextPrompter:
         <author>
         {reference.author.display_name} ({reference.author.name}) ({self.get_msg_time(reference)})
         </author>
+        You are a skilled muti-lingual translator, currently doing a translation job in a discord server. You'll get a message which you need to translate into {target_language} with context. You only need to supply the translation according to the context without any additional information. Don't act like a machine, translate smoothly like a human without being too informal. 
+        Your translation should not include the author's name and the time.
         Now is {now(tz=self.tz)}.
-        {ctx.author.display_name} ({ctx.author.name}) is asking you to translate the message in <reference> into {target_language} under the context (refer to <context>). The message is from `<author>`, so consider the context and try to understand the message before translating.
-        Refer to instructions in `<instruction>` and translate now.
+        {ctx.author.display_name} ({ctx.author.name}) is asking you to translate the message in `<reference>` into {target_language} under the context (refer to <context>). The message is from `<author>`, so consider the context and try to understand the message before translating.
         Your translation:
         """
         return prompt
