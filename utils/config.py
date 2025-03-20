e17fc8076ef610ba4df39b22f2731e858a967929
orbitoo <2098340707@qq.com>
2025-03-20 23:42:38 +0800

    实现reload方法

--- utils/config.py
+++ utils/config.py
@@ -2,17 +2,23 @@ import json
 
 CONFIG = "config.json"
 
+
 class Config:
     def __init__(self):
-        with open(CONFIG, "r", encoding="utf-8") as f:
-            self._config: dict = json.load(f)
-    
+        self._config = {}
+        self.reload()
+
     def get(self, key: str):
         return self._config.get(key)
-    
+
     def write(self, key: str, value):
         self._config[key] = value
         with open(CONFIG, "w", encoding="utf-8") as f:
             json.dump(self._config, f, indent=4)
-            
+
+    def reload(self):
+        with open(CONFIG, "r", encoding="utf-8") as f:
+            self._config = json.load(f)
+
+
 config = Config()
