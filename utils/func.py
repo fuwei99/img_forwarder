0434c687cc6d374e2ed91108c96cabb22feb52fa
orbitoo <2098340707@qq.com>
2025-03-21 15:55:35 +0800

    移动Gemini cog中的迭代器异步化函数，新增异步执行、异步线程执行

--- utils/func.py
+++ utils/func.py
@@ -1,9 +1,14 @@
+import asyncio
+import concurrent.futures
 import json
+from typing import Iterator
+import concurrent
 import pytz
 from datetime import datetime
 
 WORDS = "trigger.json"
 tz = pytz.timezone("Asia/Shanghai")
+
 
 def get_words() -> dict:
     with open(WORDS, "r", encoding="utf-8") as f:
@@ -20,5 +25,40 @@ def mapping_cog(cog_name: str) -> str:
 def now(tz=tz, fmt="%Y-%m-%d %H:%M:%S") -> str:
     return datetime.now(tz).strftime(fmt)
 
+
 def get_time(dt: datetime, tz=tz, fmt="%Y-%m-%d %H:%M:%S") -> str:
     return dt.astimezone(tz).strftime(fmt)
+
+
+async def async_iter(response: Iterator):
+    loop = asyncio.get_event_loop()
+
+    def safe_next(iterator: Iterator):
+        try:
+            return next(iterator), False
+        except StopIteration:
+            return None, True
+
+    try:
+        iterator = iter(response)
+        while True:
+            item, done = await loop.run_in_executor(None, safe_next, iterator)
+            if done:
+                break
+            yield item
+    except Exception as e:
+        print(e)
+
+
+async def async_do(func, *args, **kwargs):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, func, *args, **kwargs)
+
+
+async def async_do_thread(func, *args, **kwargs):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(func, *args, **kwargs)
+        try:
+            return await asyncio.wrap_future(future)
+        except Exception as e:
+            print(e)
